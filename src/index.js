e997363085894396d77133a5114545f0a02a22e4
Hassan Krayem <neotion@outlook.com>
2020-07-31 03:33:44 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 // Features
 // Add multiple languages to translation
 
-const ml = {
+
+/*let langs = [
+  {
+    lang: 'en',
+    translation: {
+      label: 'Label Yes',
+      music: 'Music si Love'
+    }
+  }
+]
+
+ml.addLang(langs)
+ml.setLang('en')
+console.log('devegramMl', ml.get('music'))*/
+
+export const ml = {
   lang: null,
   langs: {},
+  getFrom(l, t, d) {
+    return this.langs[l][t] || d || console.error('Devegram ML: Missing Translations for ', t, 'in ' + l + ' language') || null
+  },
   get: function (t, d) {
     return this.langs[this.lang][t] || d || console.error('Devegram ML: Missing Translations for ', t) || null
   },
@@ -13,11 +31,26 @@ const ml = {
   hasTrans: function (t) {
     return this.langs[this.lang].hasOwnProperty(t)
   },
+  addTrans: function (c) {
+    this.langs[c.lang][c.key] = c.value
+  },
   addLang: function (lang, trans) {
-    this.langs[lang] = trans
+    if (lang.constructor === Array) {      
+      lang.forEach((config) => {
+        this.langs[config.lang] = config.translation
+      })
+    } else {
+      this.langs[lang] = trans  
+    }
   },
   setLang: function (l) {
     this.lang = l
+  },
+  getLangs: function () {
+    return Object.keys(this.langs)
+  },
+  getLang: function (l) {
+    return this.langs[l]
   }
 }
 
